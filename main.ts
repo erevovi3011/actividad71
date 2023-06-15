d740cdd1d314cc003eb43162778ff6a7a93603f5
erevovi3011 <132060304+erevovi3011@users.noreply.github.com>
2023-06-15 08:52:59 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
-maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
+maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 160)
 basic.pause(2000)
 maqueen.motorStop(maqueen.Motors.M1)
 basic.pause(1000)
-maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
+maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 101)
 basic.pause(2000)
 maqueen.motorStop(maqueen.Motors.M2)
 basic.pause(2000)
-maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
+maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 103)
 maqueen.motorStop(maqueen.Motors.All)
 basic.forever(function () {
 	
